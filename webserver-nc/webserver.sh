c8e8dcda470b343d9e8231f0ea36e709506b1bc7
Henrik Sandklef <hesa@sandklef.com>
2017-02-06 10:37:07 +0100

    First rev

---
+++ webserver-nc/webserver.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+content()
+{
+    echo "<!DOCTYPE html>"
+    echo "<html>"
+    echo ""
+    echo "<head>"
+    echo "<title>Page Title</title>"
+    echo "</head>"
+    echo ""
+    echo "<body>"
+    echo "Current date is: $(date)"
+    echo "</body>"
+    echo ""
+    echo "</html>"
+}
+
+header()
+{
+    echo "HTTP/1.1 200 OK"
+    echo "Connection: close"
+    echo "Date: $(date)"
+    echo "Server: netcat special deal"
+    echo "Content-Length: $LENGTH"
+    echo "Content-Type: text/html; charset=utf-8"
+    echo "Cache-Control: max-age=60"
+    echo ""
+    echo ""
+    echo ""
+}
+
+LENGTH=$(content | wc -c)
+
+header
+content
